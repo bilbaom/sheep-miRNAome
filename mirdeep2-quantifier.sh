cd6e6b600a16ebbc6a24900df8f2f61d1ec89efb
AitorGuisa <94527621+AitorGuisa@users.noreply.github.com>
2021-11-17 17:22:23 +0100

    Update mirdeep2-quantifier.sh

--- mirdeep2-quantifier.sh
+++ mirdeep2-quantifier.sh
@@ -2,5 +2,5 @@
 
 # The quantification of known and novel miRNAs in all the samples.
 
-quantifier.pl -p /Qpremirna.fa -m /Qmature.fa -r reads/all_samples_reads.fa -y now -P -j
+quantifier.pl -p /Qallpremirna.fa -m /Qallmirna.fa -r reads/all_samples_reads.fa -y now -P -j
 
